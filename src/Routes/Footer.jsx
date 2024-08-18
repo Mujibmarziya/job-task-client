1a2c62e3f6f6792ba68f59d844e63232bc17b6ac
Marziyamujib <s03552728@gmail.com>
2024-08-18 20:19:55 +0600

    h1 deleted

--- src/Routes/Footer.jsx
+++ src/Routes/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
     </div>
   </nav> 
   <aside>
-    <p>Copyright © 2024 - All right reserved by <span className="text-xl">GadgetVillage</span> Industries Ltd</p>
+    <p>Copyright © 2023 - All right reserved by <span className="text-xl">GadgetVillage</span> Industries Ltd</p>
   </aside>
 </footer>
         </div>
