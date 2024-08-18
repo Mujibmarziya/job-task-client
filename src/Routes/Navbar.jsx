bbffd3a31b7df722c92c8748539823dc0b4e5f7e
Marziyamujib <s03552728@gmail.com>
2024-08-18 20:01:32 +0600

    navbar edited

--- src/Routes/Navbar.jsx
+++ src/Routes/Navbar.jsx
@@ -3,12 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-             <ul>
-          <li>
+        <div className=' flex gap-4 justify-around'>
+            <h1 className=' text-3xl font-bold'>Gadget Village</h1>
+             <ul className=' flex gap-2'>
+          <li className=' text-2xl text-black font-medium border-black border-2 p-3'>
           <NavLink to='/signup'>Sign Up</NavLink>
           </li>
-          <li>
+          <li className=' text-2xl text-black font-medium border-black border-2 p-3'>
           <NavLink to='/login'>Log In</NavLink>
           </li>
         </ul>
